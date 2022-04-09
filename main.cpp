8de71948831937a3b9928da49b1462210a5f467d
cakula <chatrapathi.akula@gmail.com>
2022-04-09 09:41:39 +0100

    major refactoring. use variant and optional to clean up logic

--- main.cpp
+++ main.cpp
@@ -3,83 +3,107 @@
 #include <algorithm>
 #include <optional>
 #include <cstdio>
+#include <variant>
 
-using Grid = std::vector<std::string>;
+/// Variant helper for using lambdas in-place
+template <class... Ts>
+struct visitor : Ts ... { using Ts::operator()...; };
+template <class... Ts> visitor(Ts...) -> visitor<Ts...>;
 
-enum class Direction
-{
-        R, D, L, U, Count
-};
+/// Position
+struct Position { int x, y; };
 
-constexpr auto as_int(const Direction& d)
-{
-        return static_cast<int>(d);
-}
+constexpr auto operator==(const Position& lhs, const Position& rhs) -> bool
+{ return (lhs.x == rhs.x) && (lhs.y == rhs.y); }
+
+/// Direction
+template <int D>
+struct Dir { static constexpr auto value = D; };
+
+struct R : Dir<1> {}; // Right
+struct D : Dir<1> {}; // Down
+struct L : Dir<-1> {}; // Left
+struct U : Dir<-1> {}; // Up
+
+using Direction = std::variant<R, D, L, U>;
+
+constexpr auto DirectionCount = std::variant_size_v<Direction>;
+
+struct Empty { Position pos; }; // Cell available to move into
+struct Visited { Position pos; }; // Cell has been visited previously
+struct Blocked {}; // Cell is occupied
+using Cell = std::variant<Empty, Visited, Blocked>;
 
 /**
  * @brief Map provides a thin wrapper over a Grid object to conveniently access its contents.
  */
 class Map
 {
-        const Grid& grid;
-        const int w;
-        const int h;
+    public:
+        using Layout = std::vector<std::string>;
+    private:
+        const Layout          grid;
+        int                   w, h;
+        std::vector<Position> visited;
 
     public:
-        struct Position
+        explicit Map(Layout g) : grid{std::move(g)}
         {
-                int x, y;
-        };
-
-    public:
-        explicit Map(const Grid& grid) : grid{grid}, w{static_cast<int>(grid.front().size())},
-                                         h{static_cast<int>(grid.size())}
-        {
+                w = static_cast<int>(grid.front().size());
+                h = static_cast<int>(grid.size());
+                visited.reserve(w * h);
+                visited.push_back(Position{});
         }
 
         /**
-         * @brief Obtains the value of the cell in the map at the given position.
-         * @param p Position of the cell whose value is requested.
-         * @return value of the cell if position is within bounds else std::nullopt.
+         * @brief Obtains the value of the cell in the map at the given coordinate.
+         * @param p Coordinate of the cell whose value is requested.
+         * @return Empty if '.' or Blocked if 'x'.
          */
-        constexpr auto operator()(const Position p) const -> std::optional<int>
+        auto operator()(const Position p) const -> Cell
         {
-                if (const auto[x, y] = p; (x < w && x >= 0) && (y < h && y >= 0)) {
-                        return {grid[y][x]};
-                }
-                return {};
+                const auto v = get_empty(p).value_or(Blocked{});
+                return find_visited(p).value_or(v);
         }
+
+        /**
+         * @brief Add the given position to the visited list.
+         */
+        auto mark_visited(const Position p)
+        { visited.push_back(p); }
 
         /**
          * @brief Computes the no. of cells in the map.
          */
-        [[nodiscard]]
-        auto size() const -> size_t
+        [[nodiscard]] auto count_visited() const -> size_t
+        { return visited.size(); }
+
+    private:
+        [[nodiscard]] auto find_visited(const Position& p) const -> std::optional<Cell>
         {
-                return w * h;
+                const auto pv = std::find(visited.begin(), visited.end(), p);
+                if (pv == visited.end()) { return {}; }
+                return Visited{*pv};
+        }
+
+        [[nodiscard]] auto get_empty(const Position& p) const -> std::optional<Cell>
+        {
+                const auto[x, y] = p;
+                if ((x < w && x >= 0) && (y < h && y >= 0)) {
+                        if (grid[y][x] == '.') { return Empty{p}; }
+                }
+                return {};
         }
 };
 
-constexpr auto operator+(const Map::Position& lhs, const Direction& rhs) -> Map::Position
+constexpr auto operator+(const Position& lhs, const Direction& rhs) -> Position
 {
-        constexpr int INC_DIRECTIONS[as_int(Direction::Count)]{
-                [as_int(Direction::R)] = 1,
-                [as_int(Direction::D)] = 1,
-                [as_int(Direction::L)] = -1,
-                [as_int(Direction::U)] = -1,
-        };
-
-        const auto d = INC_DIRECTIONS[as_int(rhs)];
-        const auto[x, y] = lhs;
-        if (rhs == Direction::R || rhs == Direction::L) {
-                return {x + d, y};
-        }
-        return {x, y + d};
-}
-
-constexpr auto operator==(const Map::Position& lhs, const Map::Position& rhs) -> bool
-{
-        return (lhs.x == rhs.x) && (lhs.y == rhs.y);
+        return std::visit(visitor{
+                [&lhs](const R&) -> Position { return {lhs.x + R::value, lhs.y}; },
+                [&lhs](const D&) -> Position { return {lhs.x, lhs.y + D::value}; },
+                [&lhs](const L&) -> Position { return {lhs.x + L::value, lhs.y}; },
+                [&lhs](const U&) -> Position { return {lhs.x, lhs.y + U::value}; },
+        }, rhs);
 }
 
 /**
@@ -89,73 +113,52 @@ constexpr auto operator==(const Map::Position& lhs, const Map::Position& rhs) ->
  */
 class Robot
 {
-        static constexpr Direction PEEK_DIRECTIONS[as_int(Direction::Count)]{
-                [as_int(Direction::R)] = Direction::R,
-                [as_int(Direction::D)] = Direction::D,
-                [as_int(Direction::L)] = Direction::L,
-                [as_int(Direction::U)] = Direction::U,
-        };
+        Map& map;
+        Position  pos;
+        Direction dir;
+        bool      just_visited;
+        int       nblocked;
 
     public:
-        enum class PeekStatus
-        {
-                Ok,
-                Visited,
-                Blocked,
-        };
-
-    private:
-        const Map      & map;
-        const Direction* dir;
-        Map::Position              pos;
-        std::vector<Map::Position> visited;
-
-
-    public:
-        explicit Robot(const Map& map) : dir{std::begin(PEEK_DIRECTIONS)}, pos{}, map{map}
-        {
-                visited.reserve(map.size());
-                visited.push_back(pos);
-        }
+        explicit Robot(Map& map) : map{map}, pos{}, dir{R{}}, just_visited{}, nblocked{}
+        {}
 
     public:
         /**
-         * @brief Looks ahead in the given direction.
-         * @param d Direction to peek
-         * @return new position if not visited or visited and current position if blocked.
+         * @brief Scans the cell ahead of the robot in its current direction.
          */
-        auto peek(Direction d) -> std::pair<Map::Position, PeekStatus>
+        auto peek() -> Cell
+        { return map(pos + dir); }
+
+        /**
+         * @brief Moves the robot to the given cell.
+         * @param tgt Cell to move into.
+         * @return New position if Cell is Empty or Visited and current position if Blocked.
+         */
+        auto move_to(const Cell& tgt) -> std::optional<Position>
         {
-                if (const auto _pos = pos + d; map(_pos) == '.') {
-                        if (!is_visited(_pos)) {
-                                return {_pos, PeekStatus::Ok};
+                return std::visit(visitor{
+                        [this](const Empty& e) -> std::optional<Position> {
+                            if (just_visited) { just_visited = false; }
+                            nblocked = 0;
+                            pos      = e.pos;
+                            map.mark_visited(pos);
+                            return pos;
+                        },
+                        [this](const Visited& v) -> std::optional<Position> {
+                            if (just_visited) { return {}; }
+                            just_visited = true;
+                            nblocked     = 0;
+                            pos          = v.pos;
+                            return pos;
+                        },
+                        [this](const Blocked&) -> std::optional<Position> {
+                            dir = change_direction(dir);
+                            nblocked += 1;
+                            if (nblocked == DirectionCount) { return {}; }
+                            return pos;
                         }
-
-                        return {_pos, PeekStatus::Visited};
-                }
-                return {pos, PeekStatus::Blocked};
-        }
-
-        /**
-         * @brief Updates the current position of the robot to the target.
-         * @param tgt Position to move to.
-         * @return updated position.
-         */
-        auto move_to(Map::Position tgt) -> Map::Position
-        {
-                pos = tgt;
-                return pos;
-        }
-
-        /**
-         * @brief Change the heading of the robot.
-         */
-        auto change_direction()
-        {
-                dir++;
-                if (dir == std::end(PEEK_DIRECTIONS)) {
-                        dir = std::begin(PEEK_DIRECTIONS);
-                }
+                }, tgt);
         }
 
         /**
@@ -164,50 +167,21 @@ class Robot
          */
         auto run() -> size_t
         {
-                auto just_visited = false;
-                auto nblocked     = 0;
-                do {
-                        const auto[p, status] = peek(*dir);
-
-                        if (status == PeekStatus::Visited) {
-                                nblocked = 0;
-                                // move one step ahead and peek again
-                                if (!just_visited) {
-                                        move_to(p);
-                                        just_visited = true;
-                                }
-                                else {
-                                        break;
-                                }
-                        }
-                        else if (status == PeekStatus::Ok) {
-                                nblocked = 0;
-                                if (just_visited) {
-                                        just_visited = false;
-                                }
-                                visited.push_back(move_to(p));
-                        }
-                        else {
-                                nblocked += 1;
-                                if (nblocked == as_int(Direction::Count)) {
-                                        // we're blocked in all directions
-                                        break;
-                                }
-                                change_direction();
-                        }
+                while (true) {
+                        const auto tgt = peek();
+                        if (!move_to(tgt)) { break; }
                 }
-                while (true);
-
-                return visited.size();
+                return map.count_visited();
         }
 
-    private:
-        [[nodiscard]]
-        auto is_visited(const Map::Position& p) const -> bool
+        static auto change_direction(const Direction& d) -> Direction
         {
-                return std::any_of(visited.begin(), visited.end(), [p](const auto& v) {
-                    return v == p;
-                });
+                return std::visit(visitor{
+                        [](const R&) -> Direction { return D{}; },
+                        [](const D&) -> Direction { return L{}; },
+                        [](const L&) -> Direction { return U{}; },
+                        [](const U&) -> Direction { return R{}; },
+                }, d);
         }
 };
 
@@ -216,27 +190,25 @@ auto main() -> int
         struct TestStruct
         {
                 Map map;
-                int ncleaned;
+                int ncleaned{};
         };
 
         std::vector<TestStruct> Tests{
-                TestStruct{Map{Grid{"....x..", "x......", ".....x.", "......."}}, 15},
-                TestStruct{Map{Grid{"...x..", "....xx", "..x..."}}, 6},
-                TestStruct{Map{Grid{"...x.", ".x..x", "x...x", "..x.."}}, 9},
-                TestStruct{Map{Grid{".", "."}}, 2},
-                TestStruct{Map{Grid{".x"}}, 1},
-                TestStruct{Map{Grid{".", "x"}}, 1},
+                TestStruct{Map{{"....x..", "x......", ".....x.", "......."}}, 15},
+                TestStruct{Map{{"...x..", "....xx", "..x..."}}, 6},
+                TestStruct{Map{{"...x.", ".x..x", "x...x", "..x.."}}, 9},
+                TestStruct{Map{{".", "."}}, 2},
+                TestStruct{Map{{".x"}}, 1},
+                TestStruct{Map{{".", "x"}}, 1},
         };
 
-        std::for_each(Tests.begin(), Tests.end(), [i = 0](const auto& exp) mutable {
+        std::for_each(Tests.begin(), Tests.end(), [i = 0](auto& exp) mutable {
             Robot robot{exp.map};
             std::printf("test [%d]: ", i);
             if (const auto got_ncleaned = robot.run() != exp.ncleaned) {
                     std::printf("FAIL. exp: %d, got: %d\n", exp.ncleaned, got_ncleaned);
             }
-            else {
-                    std::printf("OK\n");
-            }
+            else { std::printf("OK\n"); }
             i++;
         });
 }
